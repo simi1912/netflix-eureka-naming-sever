be2e604681a554399fc3d6f65f54b4342aba9b8f
simi1912 <nioasca@gmail.com>
2021-04-15 16:13:57 +0300

    Fixed timeout error.

--- src/test/java/com/simi/microservices2/netflixeurekanamingsever/NetflixEurekaNamingSeverApplicationTests.java
+++ src/test/java/com/simi/microservices2/netflixeurekanamingsever/NetflixEurekaNamingSeverApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NetflixEurekaNamingSeverApplicationTests {
+public class NetflixEurekaNamingSeverApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }

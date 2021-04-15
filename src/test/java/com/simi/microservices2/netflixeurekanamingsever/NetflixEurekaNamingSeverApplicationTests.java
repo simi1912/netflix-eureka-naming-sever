22f1cb4bd05e01aa4fa96acb6a3b9d43e3015f92
simi1912 <nioasca@gmail.com>
2021-04-15 11:01:18 +0300

    Initial commit

---
+++ src/test/java/com/simi/microservices2/netflixeurekanamingsever/NetflixEurekaNamingSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.netflixeurekanamingsever;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
